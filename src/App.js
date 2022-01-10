49080ef52fae30ef97634fbc20ef1b6f0791d589
neelchheda <neelchheda45@gmail.com>
2022-01-10 14:59:59 +0530

    FC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-// import SigninPage from './pages/sign';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import SigninPage from './pages/signin';
 import Home from './pages';
 
 function App() {
   return (
     <Router>
+      <Routes>
+        <Route path="/" element={Home} exact />
+        <Route path="/signin" element={SigninPage} exact />
+      </Routes>
       <Home />
       
     </Router>
