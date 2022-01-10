9269d6a2e971462643611ebf730bd5501d77a510
neelchheda <neelchheda45@gmail.com>
2022-01-10 15:47:39 +0530

    FC

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,14 +7,13 @@ const Footer = () => {
                 <FooterLinksContainer>
                     <FooterLinksWrapper>
                         <FooterLinksItems>
-                            <FooterLinksTitle>About Us</FooterLinksItems>
+                            <FooterLinksTitle>About Us</FooterLinksTitle>
                                 <FooterLinks to="/signin" >How It Works</FooterLinks>
                                 <FooterLinks to="/signin" >Testimonials</FooterLinks>
                                 <FooterLinks to="/signin" >Careers</FooterLinks>
                                 <FooterLinks to="/signin" >Investers</FooterLinks>
                                 <FooterLinks to="/signin" >Terms Of Service</FooterLinks>
-                            </FooterLinksTitle>
-                        
+                        </FooterLinksItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
             </FooterWrapper>
